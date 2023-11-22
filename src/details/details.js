3793e2b50d91feed7f3157bfca79a704ad1f927f
Saad Ahmed <137560242+Saadgithubit@users.noreply.github.com>
2023-11-22 14:11:32 +0500

    Update details.js

--- src/details/details.js
+++ src/details/details.js
@@ -1,3 +1,31 @@
-const id = window.location
+import {getSingleAdd} from "../config.js"
 
-console.log(id.search);
+addsDetails()
+
+ async function addsDetails(){
+
+    const id = window.location.search.split('=')[1]
+    console.log(id);
+    const container = document.getElementById('container')
+    const ad = await getSingleAdd(id)
+
+
+    const card = document.createElement('div')
+    const img = document.createElement('img')
+  img.src = ad.img
+  const title = document.createElement('h3')
+  title.innerHTML = ad.title
+  const amount = document.createElement('h4')
+  amount.innerHTML = ad.amount
+  card.append(img)
+  card.append(title)
+  card.append(amount)
+  container.append(card)
+//   console.log(ad);
+
+
+
+    // console.log(ad);
+    
+    
+ }
